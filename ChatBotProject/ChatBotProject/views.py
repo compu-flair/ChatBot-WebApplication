6a98fc8bb3c0adc098da347b2108792b5ad4b930
Ahmad Borzou <ahmadborzou@yahoo.com>
2024-08-14 22:32:31 -0500

    handle post requests

--- ChatBotProject/ChatBotProject/views.py
+++ ChatBotProject/ChatBotProject/views.py
@@ -1,6 +1,16 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-
+@csrf_exempt # cross site requests forgery attacks. 
 def api(request):
-    dic = {"api_response": "Hello World!"}
+    if request.method == "POST":
+        data = json.loads(request.body)
+        user_msg = data["user_msg"]
+        ## ai_msg = llm.invoke(user_msg)
+        ai_msg = "this is a temporary response from AI"
+        dic = {"api_response": ai_msg}
+        return JsonResponse(dic)
+
+    dic = {"api_response": "I cannot process GET requests. Please send a post request!"}
     return JsonResponse(dic)
