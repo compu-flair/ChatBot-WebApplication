a777b054600d7021e00243ac1080f11dd41aa395
Ahmad Borzou <ahmadborzou@yahoo.com>
2024-08-15 21:06:48 -0500

    debug turned off

--- ChatBotProject/ChatBotProject/settings.py
+++ ChatBotProject/ChatBotProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7jwj34$@w%4)u3g+epi&ohnl*5h&-kt98qy7#^jju+=sngkvv7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["159.65.165.9", "localhost"]
 
